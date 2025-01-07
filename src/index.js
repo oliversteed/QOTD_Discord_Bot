80e15701803a7af81781f3141ac64cc9e6d994e4
Oliver Steed <oliver.steed1999@gmail.com>
2025-01-07 17:14:35 +0000

    Setting time

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ client.on('interactionCreate', (interaction) => {
     }
 });
 
-const schedule = new CronJob('0 0 17 * * *', () =>{
+const schedule = new CronJob('0 0 10 * * *', () =>{
     postQOTD(client, "Dummy", false);
 });
 
